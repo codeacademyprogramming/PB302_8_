21c02e233ae1079529ed9e6e1e54f31ce0d6a561
eaghayev <eaghayevv@gmail.com>
2023-12-21 19:49:58 +0400

    Hello World changed

--- homeworks/Program.cs
+++ homeworks/Program.cs
@@ -6,7 +6,7 @@ namespace homeworks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello PB302!");
         }
     }
 }
