45b27fdf1db4ccf2dd7a124757dde1a4977b5a3a
Elchin <Elchin@MOON00>
2023-12-21 19:46:50 +0400

    Add project files.

---
+++ homeworks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace homeworks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
